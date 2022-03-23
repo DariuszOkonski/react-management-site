1c6d39f04d3632261ce00444614bec4448b81c21
Dariusz Okonski <darek200180@gmail.com>
2022-03-23 17:00:20 +0000

    build routing at app component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,43 @@
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
+import Dashboard from './pages/dashboard/Dashboard';
+import Login from './pages/login/Login';
+import Project from './pages/project/Project';
+import Signup from './pages/signup/Signup';
+import Create from './pages/create/Create';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Hello world</h1>
-      </header>
+      
+      <BrowserRouter>
+        <div className='container'>
+          <Switch>
+            
+            <Route exact path='/'>
+              <Dashboard />
+            </Route>
+
+            <Route path='/create'>
+              <Create />
+            </Route>
+
+            <Route path='/projects/:id'>
+              <Project />
+            </Route>
+
+            <Route path='/login'>
+              <Login />
+            </Route>
+
+            <Route path='/signup'>
+              <Signup />
+            </Route>
+
+          </Switch>
+        </div>      
+      </BrowserRouter>
+
     </div>
   );
 }
